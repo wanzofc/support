b44bb3b38fd1603db53d4b11c9543275a2bb2be1
wanzofc <berlianawan498@gmail.com>
2025-01-17 03:13:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
-const path = require('path'); // Menambahkan modul path untuk menangani path file
-
-const app = express();
-const PORT = 8080; // Menghapus penggunaan process.env.PORT
-
-app.use(cors());
-app.use(express.json());
-
-// Menyediakan path langsung untuk file HTML, JS, dan CSS
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'base.html')); // Mengarahkan ke base.html
-});
-
-app.get('/app.js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'app.js')); // Mengarahkan ke app.js
-});
-
-app.get('/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'style.css')); // Mengarahkan ke style.css
-});
 
 class Chatbox {
     constructor() {
@@ -105,7 +83,3 @@ class Chatbox {
 
 const chatbox = new Chatbox();
 chatbox.display();
-
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-});
